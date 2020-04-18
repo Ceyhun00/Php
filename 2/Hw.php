86b34df5bbb9a1cdd3998950323c1a975246b80c
Ceyhun00 <43810103+Ceyhun00@users.noreply.github.com>
2020-04-18 20:30:00 +0300

    Update Hw.php

--- 2/Hw.php
+++ 2/Hw.php
@@ -1,24 +1,30 @@
-
 <?php
 
-$c=$_POST['username'];
-function edit($str){
-    $count =0;
-    for ($i = 0; $i<strlen($str); $i ++) {
-        if($str[$i]=='h'){
-            $str[$i]=4;
-            $count+=1;
-        }elseif ($str[$i]=='o'){
-            $str[$i]=0;
-            $count+=1;
-        }elseif ($str[$i]=='l'){
-            $str[$i]=1;
-            $count+=1;
-        }
-    }
-    echo $str.'<br/>';
-    echo $count;
+$c=$_POST['text'];
 
+
+function simple($str,$from=0)
+{
+    $count = 0;
+    for ($i = $from; $i < strlen($str); $i++) {
+        if ($str[$i] == 'h') {
+            $str[$i] = 4;
+            $count += 1;
+        } elseif ($str[$i] == 'o') {
+            $str[$i] = 0;
+            $count += 1;
+        } elseif ($str[$i] == 'l') {
+            $str[$i] = 1;
+            $count += 1;
+        }
+        yield $str[$i];
+    }
+    echo "<br/>".$count;
 }
-edit($c);
+function pucs($str){
+   foreach (simple($str) as $val){
+    echo $val;
+   }
+}
+pucs($c);
 
