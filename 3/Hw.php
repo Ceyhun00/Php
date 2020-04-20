542296e61035d7c562b9d97468e38f45529e49f8
Ceyhun00 <43810103+Ceyhun00@users.noreply.github.com>
2020-04-20 11:35:02 +0300

    Update Hw.php

--- 3/Hw.php
+++ 3/Hw.php
@@ -1,22 +1,40 @@
 <?php
 function simple()
 {
-    $nl_char="\n";
-    $pr=explode($nl_char,$_POST['text']);
+    $nl_char = "\n";
+    $pr = explode($nl_char, $_POST['text']);
+    $arr = [];
+    $ar=[];
+    $aq = [];
     foreach ($pr as $item) {
         $words = explode(" ", $item);
         shuffle($words);
-        $result="";
-        foreach ($words as $val){
-            $result.=$val." ";
+        $result = "";
+        for ($q = 0; $q <= count($words) - 1; $q++) {
+            $result .= $words[$q]." ";
         }
-        echo $result."<br/>";
+        array_push($arr, $item, $result);
     }
-/*
-    shuffle($words);
-    foreach ($words as $val) {
-        echo $val." ";
-    }*/
+
+    for ($i = 0; $i <= count($arr) - 1; $i++) {
+        $c=explode(" ",$arr[$i]);
+        array_push($ar, $c[1]);
+    }
+    sort($ar);
+    for ($j = 0; $j <= count($arr) - 1; $j++) {
+
+        for ($b = 0; $b <= count($ar) - 1; $b++) {
+            $second=explode(" ", $arr[$b])[1];
+            $sortarrword=$ar[$j];
+            if ($sortarrword == $second) {
+                $aq[$j]=$arr[$b];
+                break;
+            }
+        }
+    }
+foreach ($aq as $j){
+    echo $j."<br/>";
+}
 }
 simple();
 
