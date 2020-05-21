8087939ccc96c4e3425ceb502b55e6b0c745ee28
Ceyhun00 <43810103+Ceyhun00@users.noreply.github.com>
2020-05-21 22:03:50 +0300

    Update index.php

--- 11/index.php
+++ 11/index.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * Index file doc comment
- * PHP version 7.*
- * 
- * @category Index
- * @package  MyPackage
- * @author   AgleevTimur <fantomas2213@gmail.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     none
- */
+
 use log\CustomLogger;
 spl_autoload_register(
     function ($class) {
